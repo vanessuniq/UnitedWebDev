ba9f0f49803fa1a991ed3a0f28ca028b000e49e3
vanessuniq <vanessuniq@gmail.com>
2020-11-08 16:32:49 -0500

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Footer from './app/Footer';
 import Header from './app/Header';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div>
         <Header/>
+        <Footer/>
       </div>
     );
   }
---
+++ src/app/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="bg-gray-200 text-center text-xs p-3 fixed bottom-0 w-full">
+            &copy; copyright React-Redux Web Dev QA Engine  Vanessa Fotso Flatiron School 2020
+        </footer>
+    );
+}
+
+export default Footer;
