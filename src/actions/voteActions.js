1151d03a8a7cffb455b65e3e50cf7911baaba21f
vanessuniq <vanessuniq@gmail.com>
2020-12-12 22:23:53 -0500

    updated likes num on button click front end

--- src/actions/voteActions.js
+++ src/actions/voteActions.js
@@ -2,12 +2,12 @@ import { postQuestionConfig } from "../helpers/configOptions";
 import { error } from "../helpers/notifications";
 import { updateQuestion } from "./questionActions";
 
-export const postVote = (post) => {
+export const postVote = (vote) => {
     return async dispatch => {
         try {
             const token = localStorage.token
             if (token) {
-                const data = await fetch('http://localhost:8080/api/v1/votes', postQuestionConfig(post, token))
+                const data = await fetch('http://localhost:8080/api/v1/votes', postQuestionConfig(vote, token))
                     .then(resp => resp.json())
                 if (data.question) {
                     dispatch(updateQuestion(data.question))
--- src/helpers/LikeButton.js
+++ src/helpers/LikeButton.js
@@ -1,18 +1,27 @@
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useState } from 'react'
+//import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { postVote } from '../actions/voteActions'
 
 export default function LikeButton({post, currentUser}) {
-    const [like, setLike ] = useState(post.votes.length)
+    const dispatch = useDispatch()
+    const likes = post.votes.length;
     const voted = post.votes.find(vote => vote.user_id === currentUser.id )
+    function addLike() {
+        const like = post.votes.find(vote => vote.user_id === currentUser.id)
+        if (!like) {
+            dispatch(postVote(post.answers? {question_id: post.id} : {answer_id: post.id}))
+        }
+    }
 
     return ( 
         <button type ='button' 
             className={`reaction-button ${voted? "text-red-500" : "text-gray-400"}`} 
-            onClick= {() => setLike(like + 1) }>
+            onClick= {addLike }>
         <FontAwesomeIcon icon={faHeart}/> &nbsp; &nbsp;
           <i className="text-sm text-green-600">
-            {`${like} ${like>1? 'likes' : 'like'}`}
+            {`${likes} ${likes>1? 'likes' : 'like'}`}
         </i>
          </button>
     )
