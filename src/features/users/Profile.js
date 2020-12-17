38f7be414d4d659ed23dc57c9bd02bdf07d89775
vanessuniq <vanessuniq@gmail.com>
2020-12-17 16:08:11 -0500

    populate user question list on profile

--- src/features/users/Profile.js
+++ src/features/users/Profile.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import TimeAgo from '../../helpers/TimeAgo'
 
 export default function Profile({user}) {
     //Design inspired by this dribbble: https://dribbble.com/shots/2316219-User-profile
     console.log(user)
+    const userQuestions = user.questions.map(question => (
+        <li key={question.id} className="grid grid-cols-3 divide-x divide-green-500 text-center">
+            <Link to={`/questions/${question.id}`}
+                className="text-purple-600 hover:text-green-600">
+                {question.title}  &nbsp;
+            </Link>
+            <h4 >{question.topic}</h4>
+            <TimeAgo timestamp={question.created_at}/>
+        </li>
+    ))
     return (
         <div 
             className="flex justify-evenly relative bg-gradient-to-r md:bg-gradient-to-l from-orange-100 via-red-300 to-pink-100 ">
@@ -27,10 +39,13 @@ export default function Profile({user}) {
                 </ul>
             </div>
             <div>
-                <h2 className="font-bold tracking-widest text-lg text-green">
+                <h2 className="font-bold tracking-widest text-lg text-center text-green mb-4">
                     Posted Questions
                     <span className="text-gray-600">({user.questions.length})</span>
                 </h2>
+                <ul className="grid grid-cols-1 divide-y divide-gray-600">
+                    {user.questions.length && userQuestions}
+                </ul>
             </div>
            
         </div>
