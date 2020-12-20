808e03400422785ee719570250d15cd42363768b
vanessuniq <vanessuniq@gmail.com>
2020-12-20 14:26:53 -0500

    adding comments to question list component

--- src/features/questions/QuestionsList.js
+++ src/features/questions/QuestionsList.js
@@ -3,15 +3,18 @@ import PostCard from '../../helpers/PostCard';
 import FilterQuestions from './FilterQuestions';
 
 const QuestionsList = ({loading, questions, currentUser, deleteQuestion}) => {
+    // state to control filtering questions based on given criteria
     const [filteredQuestions, setFilteredQuestions] = useState({
         topic: 'All topics',
         list: []
     })
+    // update the state as the list of questions from api call changes
     useEffect(() => {
        setFilteredQuestions({
            list: [...questions].sort((a,b) => b.created_at.localeCompare(a.created_at))
        })
     }, [questions])
+    // filter question list on topic select
     const onSelectChange = (e) => {
         const topic = e.target.value;
         if (topic !== 'All topics') {
@@ -30,6 +33,7 @@ const QuestionsList = ({loading, questions, currentUser, deleteQuestion}) => {
         }
         
     };
+    // filter list to match the button criteria
     const onButtonClick = (e) => {
         const list = {
             'Likes': [...questions].sort((a,b) => b.votes.length - a.votes.length),
@@ -41,6 +45,7 @@ const QuestionsList = ({loading, questions, currentUser, deleteQuestion}) => {
             list:list[e.target.innerText]
         })
     }
+    // rendering different content depending on the fetch status
     const handleQuestionsFetch = () => {
         if (loading) {
             return (
@@ -49,7 +54,7 @@ const QuestionsList = ({loading, questions, currentUser, deleteQuestion}) => {
                 </div>
             )
         } else if(questions.length !== 0) {
-            //const sortedQuestions = [...questions].sort((a,b) => b.created_at.localeCompare(a.created_at))
+            // destructuring list from state object
             const {list} = filteredQuestions;
             return list.map(question => (
                 <PostCard key={question.id} post={question} body={question.body.substring(0, 50) + ' ...'}
