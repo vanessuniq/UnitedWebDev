a0986b12e3d849ee7e7599a3b19e70297619de06
vanessuniq <vanessuniq@gmail.com>
2020-12-20 14:16:55 -0500

    complete questions filtering logic

--- src/features/questions/FilterQuestions.js
+++ src/features/questions/FilterQuestions.js
@@ -1,20 +1,25 @@
 import React from 'react'
 
-export default function FilterQuestions() {
-    const options = ['All topic','CSS', 'HTML', 'JS', 'Rails']
+export default function FilterQuestions({topic, onSelectChange, onButtonClick}) {
+    const options = ['All topics','CSS', 'HTML', 'JS', 'Rails'].map((option, index )=> (
+        <option key={index}>{option}</option>
+    ))
     return (
         <div className="flex justify-evenly border-b-2 mb-4">
             <h2 className="text-gray-500 text-2xl mb-2">questions</h2>
             <div className="mb-2">
-                <select name="topic" value="">
+                <select name="topic" className="form-select text-purple-600 w-full"
+                    value={topic} onChange={onSelectChange}>
                     {options}
                 </select>
             </div>
             <div className="mb-3">
-                <button className="reaction-button text-purple-600 hover:text-green-600">
+                <button className="reaction-button text-purple-600 hover:text-green-600"
+                    onClick={onButtonClick}>
                     Likes
                 </button>
-                <button className="reaction-button text-purple-600 hover:text-green-600">
+                <button className="reaction-button text-purple-600 hover:text-green-600"
+                    onClick={onButtonClick}>
                     Unanswered
                 </button>
             </div>
--- src/features/questions/QuestionsList.js
+++ src/features/questions/QuestionsList.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PostCard from '../../helpers/PostCard';
 import FilterQuestions from './FilterQuestions';
 
 const QuestionsList = ({loading, questions, currentUser, deleteQuestion}) => {
+    const [filteredQuestions, setFilteredQuestions] = useState({
+        topic: 'All topics',
+        list: []
+    })
+    useEffect(() => {
+       setFilteredQuestions({
+           list: [...questions].sort((a,b) => b.created_at.localeCompare(a.created_at))
+       })
+    }, [questions])
+    const onSelectChange = (e) => {
+        const topic = e.target.value;
+        if (topic !== 'All topics') {
+            setFilteredQuestions({
+                ...filteredQuestions,
+                topic: topic,
+                list: questions.filter(question => question.topic === topic)
+                                .sort((a,b) => b.created_at.localeCompare(a.created_at))
+            })
+        } else {
+            setFilteredQuestions({
+                ...filteredQuestions,
+                topic: topic,
+                list: [...questions].sort((a,b) => b.created_at.localeCompare(a.created_at))
+            })
+        }
+        
+    };
+    const onButtonClick = (e) => {
+        const list = {
+            'Likes': [...questions].sort((a,b) => b.votes.length - a.votes.length),
+            'Unanswered': [...questions].filter(question => question.answers.length === 0)
+                                        .sort((a,b) => b.created_at.localeCompare(a.created_at))
+        }
+        setFilteredQuestions({
+            ...filteredQuestions,
+            list:list[e.target.innerText]
+        })
+    }
     const handleQuestionsFetch = () => {
         if (loading) {
             return (
@@ -10,9 +48,10 @@ const QuestionsList = ({loading, questions, currentUser, deleteQuestion}) => {
                     <div className="loader"></div>
                 </div>
             )
-        } else if(questions) {
-            const sortedQuestions = [...questions].sort((a,b) => b.created_at.localeCompare(a.created_at))
-            return sortedQuestions.map(question => (
+        } else if(questions.length !== 0) {
+            //const sortedQuestions = [...questions].sort((a,b) => b.created_at.localeCompare(a.created_at))
+            const {list} = filteredQuestions;
+            return list.map(question => (
                 <PostCard key={question.id} post={question} body={question.body.substring(0, 50) + ' ...'}
                     currentUser={currentUser} deleteQuestion={deleteQuestion}
                 />
@@ -24,7 +63,11 @@ const QuestionsList = ({loading, questions, currentUser, deleteQuestion}) => {
     return (
         <section className='posts-list mb-8'>
             <h1 className="font-bold text-2xl mb-3">All Questions</h1>
-            <FilterQuestions/>
+            <FilterQuestions 
+                topic={filteredQuestions.topic} 
+                onSelectChange={onSelectChange} 
+                onButtonClick={onButtonClick}
+            />
             {handleQuestionsFetch()}
         </section>
     );
